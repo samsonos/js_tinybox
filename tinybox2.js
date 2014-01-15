8bb3e9d20cf3763dfe3e1e1bd044efb95febd67a
vitalyiegorov <egorov@samsonos.com>
2014-01-15 14:04:49 +0200

    Added beforeHandler and responseHandler to tinyboxAjax, they can stop execution by returning false

--- tinybox2.js
+++ tinybox2.js
@@ -9,7 +9,7 @@ var sjstinybox = {
      *
      * @param handler
      */
-    tinyboxAjax : function(handler){
+    tinyboxAjax : function(responseHandler, beforeHandler){
         // Iterate all DOM elements in current selection
         return this.each(function(elm, respContainerName)
         {
@@ -19,15 +19,22 @@ var sjstinybox = {
             // Bind asynchronous request on element click
             elm.ajaxClick(function(response, clickable){
 
-                var handler =
+                // Call beforeHandler if passed and save return value, default - true
+                var beforeStatus = beforeHandler != undefined ? beforeHandler(elm) : true;
+
                 // If we recieved html container
-                if (response[respContainerName] != undefined) {
-                    // Create SamsonJS object
-                    var form = s(response[respContainerName]);
-                    // Append response to body
-                    s(document.body).append(form);
-                    // Show tinybox
-                    var tb = tinybox(form);
+                if (beforeStatus && response[respContainerName] != undefined) {
+                    // Call responseHandler if passed and save return value, default - true
+                    var responseStatus = responseHandler != undefined ? responseHandler(elm) : true;
+                    // If response handler succeded - show tiny box
+                    if (responseStatus) {
+                        // Create SamsonJS object
+                        var form = s(response[respContainerName]);
+                        // Append response to body
+                        s(document.body).append(form);
+                        // Show tinybox
+                        var tb = tinybox(form);
+                    }
                 }
             });
         });
